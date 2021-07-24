07853b3248731dff798000890cbed199838cc705
blinasmailaj <74669741+blinasmailaj@users.noreply.github.com>
2021-07-24 03:44:14 +0200

    User Login Form

---
+++ Projekti2/UDP/UserLogin.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace UDP
+{
+    partial class UserLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserLogin));
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.successLabel = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.registerHereLabel = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.AutoSize = true;
+            this.msgLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.msgLabel.ForeColor = System.Drawing.Color.Red;
+            this.msgLabel.Location = new System.Drawing.Point(175, 24);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(0, 17);
+            this.msgLabel.TabIndex = 35;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(44, 161);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(257, 45);
+            this.btnLogin.TabIndex = 34;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // successLabel
+            // 
+            this.successLabel.AutoSize = true;
+            this.successLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.successLabel.ForeColor = System.Drawing.Color.Lime;
+            this.successLabel.Location = new System.Drawing.Point(148, 238);
+            this.successLabel.Name = "successLabel";
+            this.successLabel.Size = new System.Drawing.Size(0, 17);
+            this.successLabel.TabIndex = 33;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(154, 24);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 17);
+            this.errorLabel.TabIndex = 32;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(41, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 24);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "User Login";
+            // 
+            // registerHereLabel
+            // 
+            this.registerHereLabel.AutoSize = true;
+            this.registerHereLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline);
+            this.registerHereLabel.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.registerHereLabel.Location = new System.Drawing.Point(42, 127);
+            this.registerHereLabel.Name = "registerHereLabel";
+            this.registerHereLabel.Size = new System.Drawing.Size(259, 17);
+            this.registerHereLabel.TabIndex = 30;
+            this.registerHereLabel.Text = "Dont have an account ? Register Here .";
+            this.registerHereLabel.Click += new System.EventHandler(this.registerHereLabel_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtPassword.Location = new System.Drawing.Point(141, 90);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(160, 23);
+            this.txtPassword.TabIndex = 29;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(41, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtUsername.Location = new System.Drawing.Point(141, 50);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(160, 23);
+            this.txtUsername.TabIndex = 27;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(41, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Username:";
+            // 
+            // UserLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.ClientSize = new System.Drawing.Size(341, 218);
+            this.Controls.Add(this.msgLabel);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.successLabel);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.registerHereLabel);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UserLogin";
+            this.Text = "UserLogin";
+            this.Load += new System.EventHandler(this.UserLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label msgLabel;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label successLabel;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label registerHereLabel;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Projekti2/UDP/UserLogin.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Security.Cryptography;
+using System.Net;
+
+
+namespace UDP
+{
+    public partial class UserLogin : Form
+    {
+        public event EventHandler ButtonFirstFormClicked;
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+        public string username = string.Empty;
+        public string password = string.Empty;
+        public bool loginclicked = false;
+
+        private void registerHereLabel_Click(object sender, EventArgs e)
+        {
+            UserRegistration userRegistration = new UserRegistration();
+            userRegistration.ShowDialog();
+        }
+
+        public void btnLogin_Click(object sender, EventArgs e)
+        {
+            loginclicked = true;
+            XmlDocument doc = new XmlDocument();
+
+            doc.Load("users.xml");
+
+            foreach (XmlNode node in doc.SelectNodes("/users/user"))
+            {
+                String Username = node.SelectSingleNode("username").InnerText;
+                String Password = node.SelectSingleNode("password").InnerText;
+
+                if (String.IsNullOrEmpty(txtUsername.Text) || String.IsNullOrEmpty(txtPassword.Text))
+                {
+                    MessageBox.Show("All Fields must be filled with information!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+
+                string Salt = node.SelectSingleNode("salt").InnerText;
+
+                string SaltedPassword = Salt + txtPassword.Text;
+                string SaltedHashPassword = CalculateHash(SaltedPassword);
+
+                if (Username == txtUsername.Text && Password == SaltedHashPassword)
+                {
+                    MessageBox.Show("OK", "Success", MessageBoxButtons.OK, MessageBoxIcon.None);
+                    UserPage userPage = new UserPage(txtUsername.Text);
+                    userPage.ShowDialog();
+                }
+
+
+                //MessageBox.Show("ERROR" , "Wrong",MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+            }
+        }
+        private String CalculateHash(string SaltedPassword)
+        {
+            byte[] byteSaltedPassword = Encoding.UTF8.GetBytes(SaltedPassword);
+
+            SHA1CryptoServiceProvider objHash = new SHA1CryptoServiceProvider();
+            byte[] byteSaltedHashPassword = objHash.ComputeHash(byteSaltedPassword);
+
+            return Convert.ToBase64String(byteSaltedHashPassword);
+        }
+
+        private void UserLogin_Load(object sender, EventArgs e)
+        {
+            
+        }
+        public string getUsername()
+        {
+            username = txtUsername.Text.ToString();
+            return username;
+        }
+        public string getPassword()
+        {
+            username = txtUsername.Text.ToString();
+            return username;
+        }
+
+        internal void btnLogin_Click()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
