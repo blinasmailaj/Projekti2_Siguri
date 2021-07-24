34753da3c655b007cfee9f959607cc48e43756e4
blinasmailaj <74669741+blinasmailaj@users.noreply.github.com>
2021-07-24 03:43:46 +0200

    Forma per shtimin e fatures

---
+++ Projekti2/UDP/Fatura.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace UDP
+{
+    partial class Fatura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fatura));
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNrFatures = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtShuma = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtViti = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMuaji = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLlojiFatures = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(22, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 60;
+            this.label6.Text = "User :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(48, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 24);
+            this.label5.TabIndex = 59;
+            this.label5.Text = "Fature e re";
+            // 
+            // txtNrFatures
+            // 
+            this.txtNrFatures.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtNrFatures.Location = new System.Drawing.Point(149, 129);
+            this.txtNrFatures.Name = "txtNrFatures";
+            this.txtNrFatures.Size = new System.Drawing.Size(160, 23);
+            this.txtNrFatures.TabIndex = 58;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label.ForeColor = System.Drawing.Color.White;
+            this.label.Location = new System.Drawing.Point(49, 131);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(83, 17);
+            this.label.TabIndex = 57;
+            this.label.Text = "Nr. Fatures:";
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.userNameLabel.ForeColor = System.Drawing.Color.White;
+            this.userNameLabel.Location = new System.Drawing.Point(68, 19);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(12, 17);
+            this.userNameLabel.TabIndex = 56;
+            this.userNameLabel.Text = ".";
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.AutoSize = true;
+            this.msgLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.msgLabel.ForeColor = System.Drawing.Color.Red;
+            this.msgLabel.Location = new System.Drawing.Point(49, 72);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(0, 17);
+            this.msgLabel.TabIndex = 55;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnAdd.Location = new System.Drawing.Point(52, 284);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(257, 45);
+            this.btnAdd.TabIndex = 54;
+            this.btnAdd.Text = "Shto Faturen";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtShuma
+            // 
+            this.txtShuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtShuma.Location = new System.Drawing.Point(149, 249);
+            this.txtShuma.Name = "txtShuma";
+            this.txtShuma.Size = new System.Drawing.Size(160, 23);
+            this.txtShuma.TabIndex = 53;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(49, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 17);
+            this.label4.TabIndex = 52;
+            this.label4.Text = "Vlera ne Euro:";
+            // 
+            // txtViti
+            // 
+            this.txtViti.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtViti.Location = new System.Drawing.Point(149, 169);
+            this.txtViti.Name = "txtViti";
+            this.txtViti.Size = new System.Drawing.Size(160, 23);
+            this.txtViti.TabIndex = 51;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(49, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 17);
+            this.label3.TabIndex = 50;
+            this.label3.Text = "Viti:";
+            // 
+            // txtMuaji
+            // 
+            this.txtMuaji.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtMuaji.Location = new System.Drawing.Point(149, 209);
+            this.txtMuaji.Name = "txtMuaji";
+            this.txtMuaji.Size = new System.Drawing.Size(160, 23);
+            this.txtMuaji.TabIndex = 49;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(49, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "Muaji:";
+            // 
+            // txtLlojiFatures
+            // 
+            this.txtLlojiFatures.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtLlojiFatures.Location = new System.Drawing.Point(149, 89);
+            this.txtLlojiFatures.Name = "txtLlojiFatures";
+            this.txtLlojiFatures.Size = new System.Drawing.Size(160, 23);
+            this.txtLlojiFatures.TabIndex = 47;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(49, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Lloji Fatures:";
+            // 
+            // Fatura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(355, 361);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNrFatures);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.msgLabel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtShuma);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtViti);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMuaji);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLlojiFatures);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Fatura";
+            this.Text = "Regjistrim i Fatures ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNrFatures;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label msgLabel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtShuma;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtViti;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMuaji;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLlojiFatures;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Projekti2/UDP/Fatura.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace UDP
+{
+    public partial class Fatura : Form
+    {
+        public Fatura(string username)
+        {
+            InitializeComponent();
+            userNameLabel.Text = username;
+        }
+        XmlDocument xml = new XmlDocument();
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            xml.Load("users.xml");
+            XmlNode userNode = xml.SelectSingleNode("users/user[@id='" + userNameLabel.Text + "']");
+
+            XmlElement shpenzimetNode = xml.CreateElement("shpenzimet");
+            XmlElement faturaNode = xml.CreateElement("fatura");
+            XmlElement llojiFaturesNode = xml.CreateElement("llojiFatures");
+            XmlElement nrFaturesNode = xml.CreateElement("nrFatures");
+            XmlElement vitiNode = xml.CreateElement("viti");
+            XmlElement muajiNode = xml.CreateElement("muaji");
+            XmlElement vleraNode = xml.CreateElement("vleraNeEuro");
+
+            if (String.IsNullOrEmpty(txtLlojiFatures.Text) || String.IsNullOrEmpty(txtNrFatures.Text) || String.IsNullOrEmpty(txtViti.Text) || String.IsNullOrEmpty(txtMuaji.Text) || String.IsNullOrEmpty(txtShuma.Text))
+            {
+                MessageBox.Show("All Fields must be filled with information!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            llojiFaturesNode.InnerText = txtLlojiFatures.Text;
+            nrFaturesNode.InnerText = txtNrFatures.Text;
+            vitiNode.InnerText = txtViti.Text;
+            muajiNode.InnerText = txtMuaji.Text;
+            vleraNode.InnerText = txtShuma.Text;
+
+            faturaNode.AppendChild(llojiFaturesNode);
+            faturaNode.AppendChild(nrFaturesNode);
+            faturaNode.AppendChild(vitiNode);
+            faturaNode.AppendChild(muajiNode);
+            faturaNode.AppendChild(vleraNode);
+            shpenzimetNode.AppendChild(faturaNode);
+
+            userNode.AppendChild(shpenzimetNode);
+
+            xml.Save("users.xml");
+
+        }
+    }
+    
+}
