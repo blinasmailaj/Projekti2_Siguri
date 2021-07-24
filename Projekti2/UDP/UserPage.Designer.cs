2fcc6dc2fd1effccf05332fc8facf51ad1dae98f
blinasmailaj <74669741+blinasmailaj@users.noreply.github.com>
2021-07-24 03:44:47 +0200

    User Page Form

---
+++ Projekti2/UDP/UserPage.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace UDP
+{
+    partial class UserPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserPage));
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnShto = new System.Windows.Forms.Button();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnRefresh.ForeColor = System.Drawing.Color.White;
+            this.btnRefresh.Location = new System.Drawing.Point(12, 252);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(161, 41);
+            this.btnRefresh.TabIndex = 14;
+            this.btnRefresh.Text = "Reload";
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Pershendetje";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Te dhenat e shfrytezuesit :";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.DarkCyan;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.DarkCyan;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView.GridColor = System.Drawing.Color.White;
+            this.dataGridView.Location = new System.Drawing.Point(12, 92);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(499, 152);
+            this.dataGridView.TabIndex = 11;
+            // 
+            // btnShto
+            // 
+            this.btnShto.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnShto.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnShto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnShto.ForeColor = System.Drawing.Color.White;
+            this.btnShto.Location = new System.Drawing.Point(179, 252);
+            this.btnShto.Name = "btnShto";
+            this.btnShto.Size = new System.Drawing.Size(332, 41);
+            this.btnShto.TabIndex = 10;
+            this.btnShto.Text = "Shto Shpenzime te tjera";
+            this.btnShto.UseVisualStyleBackColor = false;
+            this.btnShto.Click += new System.EventHandler(this.btnShto_Click);
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.userNameLabel.ForeColor = System.Drawing.Color.White;
+            this.userNameLabel.Location = new System.Drawing.Point(109, 25);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(99, 20);
+            this.userNameLabel.TabIndex = 9;
+            this.userNameLabel.Text = "Blina Smailaj";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(130, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 8;
+            // 
+            // UserPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.ClientSize = new System.Drawing.Size(523, 305);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnShto);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UserPage";
+            this.Text = "UserPage";
+            this.Load += new System.EventHandler(this.UserPage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnShto;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Projekti2/UDP/UserPage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+namespace UDP
+{
+    public partial class UserPage : Form
+    {
+        public UserPage(string stringValue)
+        {
+            InitializeComponent();
+            userNameLabel.Text = stringValue;
+        }
+
+        private void UserPage_Load(object sender, EventArgs e)
+        {
+            GetXMLData();
+        }
+
+        private void btnShto_Click(object sender, EventArgs e)
+        {
+            Fatura fatura = new Fatura(userNameLabel.Text);
+            fatura.Show();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            GetXMLData();
+        }
+        protected void GetXMLData()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("LLoji i Fatures", typeof(string));
+            dt.Columns.Add("Numri i Fatures", typeof(string));
+            dt.Columns.Add("Viti", typeof(string));
+            dt.Columns.Add("Muaji", typeof(string));
+            dt.Columns.Add("Vlera", typeof(string));
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load("users.xml");
+            XmlNodeList nodeList = xmldoc.SelectNodes("/users/user[@id=\"" + userNameLabel.Text + "\"]/shpenzimet/fatura");
+            foreach (XmlNode node in nodeList)
+            {
+                DataRow dtrow = dt.NewRow();
+                dtrow["LLoji i Fatures"] = node["llojiFatures"].InnerText;
+                dtrow["Numri i Fatures"] = node["nrFatures"].InnerText;
+                dtrow["Viti"] = node["viti"].InnerText;
+                dtrow["Muaji"] = node["muaji"].InnerText;
+                dtrow["Vlera"] = node["vleraNeEuro"].InnerText;
+
+                dt.Rows.Add(dtrow);
+            }
+            dataGridView.DataSource = dt;
+
+
+            //gvDetails.DataBind();
+        }
+    }
+}
