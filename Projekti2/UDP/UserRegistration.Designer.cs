569c1c2a264e8026c890360f3f72372b9ca7d05e
blinasmailaj <74669741+blinasmailaj@users.noreply.github.com>
2021-07-24 03:45:18 +0200

    User Registration Form

---
+++ Projekti2/UDP/UserRegistration.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace UDP
+{
+    partial class UserRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserRegistration));
+            this.txtCPassword = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBirthYear = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCPassword
+            // 
+            this.txtCPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtCPassword.Location = new System.Drawing.Point(130, 289);
+            this.txtCPassword.Name = "txtCPassword";
+            this.txtCPassword.Size = new System.Drawing.Size(160, 23);
+            this.txtCPassword.TabIndex = 47;
+            this.txtCPassword.UseSystemPasswordChar = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(29, 295);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 17);
+            this.label7.TabIndex = 46;
+            this.label7.Text = "Confirm Pass:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtPassword.Location = new System.Drawing.Point(130, 248);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(160, 23);
+            this.txtPassword.TabIndex = 45;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(30, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 17);
+            this.label8.TabIndex = 44;
+            this.label8.Text = "Password:";
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.AutoSize = true;
+            this.msgLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.msgLabel.ForeColor = System.Drawing.Color.Red;
+            this.msgLabel.Location = new System.Drawing.Point(30, 31);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(0, 17);
+            this.msgLabel.TabIndex = 43;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(29, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(152, 24);
+            this.label6.TabIndex = 42;
+            this.label6.Text = "User Registration";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtUsername.Location = new System.Drawing.Point(130, 208);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(160, 23);
+            this.txtUsername.TabIndex = 41;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(30, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 17);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "Username:";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.btnRegister.ForeColor = System.Drawing.Color.White;
+            this.btnRegister.Location = new System.Drawing.Point(33, 330);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(257, 45);
+            this.btnRegister.TabIndex = 39;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtGender
+            // 
+            this.txtGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtGender.Location = new System.Drawing.Point(130, 168);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(160, 23);
+            this.txtGender.TabIndex = 38;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(30, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "Gender:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtLastName.Location = new System.Drawing.Point(130, 88);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(160, 23);
+            this.txtLastName.TabIndex = 36;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(30, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "Last Name:";
+            // 
+            // txtBirthYear
+            // 
+            this.txtBirthYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtBirthYear.Location = new System.Drawing.Point(130, 128);
+            this.txtBirthYear.Name = "txtBirthYear";
+            this.txtBirthYear.Size = new System.Drawing.Size(160, 23);
+            this.txtBirthYear.TabIndex = 34;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(30, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Birth Year:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtFirstName.Location = new System.Drawing.Point(130, 48);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(160, 23);
+            this.txtFirstName.TabIndex = 32;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(30, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "First Name:";
+            // 
+            // UserRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.ClientSize = new System.Drawing.Size(319, 389);
+            this.Controls.Add(this.txtCPassword);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.msgLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBirthYear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UserRegistration";
+            this.Text = "User Registration Form";
+            this.Load += new System.EventHandler(this.UserRegistration_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCPassword;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label msgLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBirthYear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Projekti2/UDP/UserRegistration.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Xml;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace UDP
+{
+    public partial class UserRegistration : Form
+    {
+        public bool registerclick = false;
+        public UserRegistration()
+        {
+            InitializeComponent();
+        }
+        public string firstname = string.Empty;
+        public string lastname = string.Empty;
+        public string birthyear = string.Empty;
+        public string gender = string.Empty;
+        public string username = string.Empty;
+        public string password = string.Empty;
+
+
+        private void UserRegistration_Load(object sender, EventArgs e)
+        {
+
+        }
+        XmlDocument objXml = new XmlDocument();
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMiniMaxChars = new Regex(@".{8,15}");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+            registerclick = true;
+            string Salt = new Random().Next(100000, 1000000).ToString();
+            string SaltedPassword = Salt + txtPassword.Text;
+
+            string SaltedHashPassword = CalculateHash(SaltedPassword);
+            if (File.Exists("users.xml") == false)
+            {
+                XmlTextWriter xmlTw = new XmlTextWriter("users.xml", Encoding.UTF8);
+                xmlTw.WriteStartElement("users");
+                xmlTw.Close();
+            }
+            objXml.Load("users.xml");
+            XmlElement rootNode = objXml.DocumentElement;
+
+            XmlElement userNode = objXml.CreateElement("user");
+            XmlElement firstNameNode = objXml.CreateElement("firstName");
+            XmlElement lastNameNode = objXml.CreateElement("lastName");
+            XmlElement genderNode = objXml.CreateElement("gender");
+            XmlElement birthYearNode = objXml.CreateElement("birthYear");
+            XmlElement usernameNode = objXml.CreateElement("username");
+            XmlElement passwordNode = objXml.CreateElement("password");
+            XmlElement saltNode = objXml.CreateElement("salt");
+
+            XmlNodeList nodes = rootNode.SelectNodes("user/username");
+
+            foreach (XmlNode node in nodes)
+            {
+                if (node.InnerText == txtUsername.Text)
+                {
+                    MessageBox.Show("User already exists!");
+                    return;
+                }
+            }
+
+            if (txtPassword.Text != txtCPassword.Text)
+            {
+                MessageBox.Show("Passwords do not match!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else if (String.IsNullOrEmpty(txtFirstName.Text) || String.IsNullOrEmpty(txtGender.Text) || String.IsNullOrEmpty(txtLastName.Text) || String.IsNullOrEmpty(txtBirthYear.Text) || String.IsNullOrEmpty(txtUsername.Text) || String.IsNullOrEmpty(txtPassword.Text) || String.IsNullOrEmpty(txtCPassword.Text))
+            {
+                MessageBox.Show("All Fields must be filled with information!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else if (!hasLowerChar.IsMatch(txtPassword.Text))
+            {
+                MessageBox.Show("Password should contain At least one lower case letter", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            else if (!hasUpperChar.IsMatch(txtPassword.Text))
+            {
+                MessageBox.Show("Password should contain At least one upper case letter", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            else if (!hasMiniMaxChars.IsMatch(txtPassword.Text))
+            {
+                MessageBox.Show("Password should not be less than or greater than 12 characters", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (!hasSymbols.IsMatch(txtPassword.Text))
+            {
+                MessageBox.Show("Password should contain At least one special case characters", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            
+
+
+
+
+            firstNameNode.InnerText = txtFirstName.Text;
+            lastNameNode.InnerText = txtLastName.Text;
+            genderNode.InnerText = txtGender.Text;
+            birthYearNode.InnerText = txtBirthYear.Text;
+            usernameNode.InnerText = txtUsername.Text;
+            passwordNode.InnerText = SaltedHashPassword;
+            saltNode.InnerText = Salt;
+            userNode.SetAttribute("id", txtUsername.Text);
+            userNode.AppendChild(firstNameNode);
+            userNode.AppendChild(lastNameNode);
+            userNode.AppendChild(birthYearNode);
+            userNode.AppendChild(genderNode);
+            userNode.AppendChild(usernameNode);
+            userNode.AppendChild(passwordNode);
+            userNode.AppendChild(saltNode);
+
+            rootNode.AppendChild(userNode);
+
+            objXml.Save("users.xml");
+            MessageBox.Show("OK", "Success", MessageBoxButtons.OK, MessageBoxIcon.None);
+
+
+
+
+
+
+
+        }
+
+        private String CalculateHash(string SaltedPassword)
+        {
+            byte[] byteSaltedPassword = Encoding.UTF8.GetBytes(SaltedPassword);
+
+            SHA1CryptoServiceProvider objHash = new SHA1CryptoServiceProvider();
+            byte[] byteSaltedHashPassword = objHash.ComputeHash(byteSaltedPassword);
+
+            return Convert.ToBase64String(byteSaltedHashPassword);
+        }
+
+        public string getUsername()
+        {
+            username = txtUsername.Text.ToString();
+            return username;
+        }
+        public string getPassword()
+        {
+            password = txtPassword.Text.ToString();
+            return username;
+        }
+        public string getBirthYear()
+        {
+            birthyear = txtBirthYear.Text.ToString();
+            return username;
+        }
+        public string getGender()
+        {
+            gender = txtGender.Text.ToString();
+            return username;
+        }
+        public string getFirstName()
+        {
+            firstname = txtFirstName.Text.ToString();
+            return username;
+        }
+        public string getLastName()
+        {
+            lastname = txtLastName.Text.ToString();
+            return username;
+        }
+
+    }
+}
+
